7b72d4be00bc7c480ea5619c1075be45d0f5cf33
Sorina Mancas <sorinam@yahoo.com>
2016-04-21 17:26:09 +0300

    install\uninstall

---
+++ FileMonitorSilent/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMonitorFullSilentUninstall
+{
+    class Program
+    {
+        const string productName = "Temasoft FileMonitor Server";
+        static void Main(string[] args)
+        {
+            var uninstallString = GetUninstallStringOfApplication(productName);
+
+            if (uninstallString != "")
+            {
+                Console.WriteLine("Application '{0}' is installed .", productName);
+                Console.WriteLine("Unnstall String is: {0}", uninstallString);
+                RunUninstallCommand(uninstallString);
+
+            }
+            else
+                Console.WriteLine("Application '{0}' is not installed!", productName);
+            Console.ReadKey();
+        }
+
+        private static void RunUninstallCommand(string uninstallString)
+        {
+            string arguments = " /quiet /norestart ";
+            //Console.WriteLine("Arguments:{0}",uninstallString+ arguments);
+            string parameters = uninstallString.Substring(11) + arguments;
+            try
+            {
+                var process = Process.Start("MsiExec.exe", parameters);
+                process.WaitForExit();
+                Console.WriteLine("Exit Code: {0} ", process.ExitCode);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Message: {0} ", e.Message);
+            }
+        }
+
+        public static string GetUninstallStringOfApplication(string productName)
+        {
+            string result = "";
+            string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(uninstallKey))
+            {
+                foreach (string skName in registryKey.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = registryKey.OpenSubKey(skName))
+                    {
+                        try
+                        {
+                            if (subKey.GetValue("DisplayName").ToString() == productName)
+                            {
+                                result = subKey.GetValue("UninstallString").ToString();
+                                return result;
+                            };
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ FileMonitorSilentInstall/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace FileMonitorSilentInstall
+{
+    class Program
+    {
+        const string logFile = @"SilentInstall.txt";
+        const string serverInstallerKit = "ServerInstaller.exe";
+        const string installConfigFile = "silentinstallconfig.xml";
+        const string processConfigName = "PostInstallServerConfig";
+        const string postInstallConfigFile = @"C:\Program Files (x86)\Temasoft\FileMonitor Server\PostInstallServerConfig.exe";
+
+        static void Main(string[] args)
+        {
+            switch (args.Count())
+            {
+                case 0:
+                    Console.WriteLine("Please specify a folder path containing ServerInstaller and configuration file!");
+                    break;
+                case 1:
+                    RunServerInstaller(args[0]);
+                    break;
+                default:
+                    Console.WriteLine("Please provide only one parameter!");
+                    break;
+            }
+        }
+
+        private static void RunServerInstaller(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                RunServerInstallerKit(path);
+            }
+            else
+            {
+                var logMessages = "This path: " + path + " doesn't exists";
+                WriteLogsIntoAFile(logMessages);
+            }
+        }
+
+        private static void RunServerInstallerKit(string path)
+        {
+            string fullPathServerInstaller = Path.Combine(path, serverInstallerKit);
+            string fullPathInstallConfig = Path.Combine(path, installConfigFile);
+            string programDataInstallConfig = Path.Combine(@"c:\ProgramData", installConfigFile);
+
+            if (System.IO.File.Exists(fullPathServerInstaller) && System.IO.File.Exists(fullPathInstallConfig))
+            {
+                PerformCommands(fullPathServerInstaller, fullPathInstallConfig, programDataInstallConfig);
+            }
+            else
+            {
+                var errorMessage = "Some needed file are missing";
+                WriteLogsIntoAFile(errorMessage);
+            }
+        }
+
+        private static void PerformCommands(string fullPathServerInstaller, string fullPathInstallConfig, string programDataInstallConfig)
+        {
+            if (File.Exists(programDataInstallConfig))
+            {
+                File.Delete(programDataInstallConfig);
+            }
+
+            Process.Start(fullPathServerInstaller, "/i /q");
+
+            bool proceessIsRunning = WaitUntilConfigProcessRunning();
+
+            if (proceessIsRunning)
+            {
+                GetAllProcessesAndKillConfigProcess(processConfigName);
+                File.Copy(fullPathInstallConfig, programDataInstallConfig, true);
+
+                Thread.Sleep(150000);
+
+                //Process.Start(postInstallConfigFile);
+                var result = LaunchProcessAndWaitForProcessToFinish(postInstallConfigFile, 60000);
+                if (result != 0)
+                {
+                    var errorMessage = "An error was occured on PostInstallConfig or timed out!!";
+                    WriteLogsIntoAFile(errorMessage);
+                }
+            }
+            else
+            {
+                var errorMessage = "Something was wrong or timed out!!";
+                WriteLogsIntoAFile(errorMessage);
+            }
+        }
+
+        private static bool WaitUntilConfigProcessRunning()
+        {
+            bool found = false;
+            int count = 0;
+            while (!found && count < 100)
+            {
+                var proceses = Process.GetProcessesByName(processConfigName);
+
+                if (proceses.Count() > 0)
+                {
+                    found = true;
+                }
+
+                Thread.Sleep(1000);
+                count++;
+            }
+
+            return found;
+        }
+
+        private static void GetAllProcessesAndKillConfigProcess(string processName)
+        {
+            var proceses = Process.GetProcessesByName(processName);
+
+            if (proceses.Count() > 0)
+            {
+                foreach (Process proces in proceses)
+                {
+                    KillProcess(proces);
+                }
+            }
+            else
+            {
+                WriteLogsIntoAFile("Process " + processName + " is not running");
+            }
+        }
+
+        private static void KillProcess(Process proces)
+        {
+            try
+            {
+                proces.Kill();
+            }
+            catch (Exception e)
+            {
+                WriteLogsIntoAFile(e.Message);
+            }
+        }
+
+        private static int LaunchProcessAndWaitForProcessToFinish(string commandLine, int timeToWaitForProcessToFinishInMilliseconds = 0)
+        {
+            int result = 0;
+
+            try
+            {
+                ProcessStartInfo startinfo = new ProcessStartInfo(commandLine);
+                startinfo.WindowStyle = ProcessWindowStyle.Normal;
+                //startinfo.UseShellExecute = true;
+                Process p = Process.Start(commandLine, "/install");
+                if (timeToWaitForProcessToFinishInMilliseconds == 0)
+                {
+                    p.WaitForExit();
+                }
+                else
+                {
+                    p.WaitForExit(timeToWaitForProcessToFinishInMilliseconds);
+                }
+
+                if (p.HasExited)
+                {
+                    result = p.ExitCode;
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLogsIntoAFile("LaunchProcessAndWaitForProcessToFinish : " + e.Message);
+            }
+
+            return result;
+        }
+
+        private static void WriteLogsIntoAFile(string LogMessage)
+        {
+            if (System.IO.Directory.Exists(logFile))
+            {
+                File.AppendAllText(logFile, LogMessage);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(logFile, LogMessage);
+            }
+        }
+    }
+}
